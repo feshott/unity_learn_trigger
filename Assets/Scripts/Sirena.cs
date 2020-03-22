c9369a0c276ef01076f541fbc13b6ecaae2075b1
feshott <feshot@yandex.ru>
2020-03-22 14:16:54 +0300

    first commit

---
+++ Assets/Scripts/Sirena.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sirena : MonoBehaviour
+{
+    private AudioSource _sirenaSound;
+    private bool _sirenaActive = false;
+
+    private void Start()
+    {
+        _sirenaSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _sirenaActive = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        _sirenaActive = false;
+    }
+
+    private void Update()
+    {
+        if (_sirenaActive && _sirenaSound.volume < 1) 
+        {
+            _sirenaSound.volume += Time.deltaTime/10;
+        }
+        if (!_sirenaActive && _sirenaSound.volume>0)
+        {
+            _sirenaSound.volume -= Time.deltaTime / 10;
+        }
+    }
+}
